f9fcd79cc9c97f42de5cad9e18931aa7922e34a3
towa <football.keepergrove2@gmail.com>
2024-02-04 22:33:03 +0900

    for文

---
+++ hb.py
@@ -0,0 +1,53 @@
+import random
+
+def hb(num):
+    ans = str(random.randint(10 ** (num - 1), 10 ** (num) - 1))
+    print(ans)
+    
+    ans1 = [ans[i:i+1] for i in range(num)]
+    print(ans1)
+    
+    ans2 = ''.join(ans)
+    
+    print(f'数あてゲームを始めるよ! 答えだと思う{num}桁の数値を入れてね!')
+    for _ in range(100): 
+        image = input()
+        image1 = [image[i:i+1] for i in range(num)]
+        
+        if ans1 == image1:
+            print(f'正解、すごい!')
+            break
+        
+        elif ans1 != image1:
+            n = 0
+            m = 0
+            j = 0
+            for j in range(num):
+                if image1[j] in ans1 and image1[k] in ans1:
+                    index1 = ans1.index(image1[j])
+                    index2 = ans1.index(image1[k])
+                    
+                    if index1 == index2 == j:
+                        n += 1
+                    
+                    elif index1 == j and index2 == k and j != k:
+                        n += 2
+                    
+                    elif index1 == j and index2 != k and index1 != index2:
+                        n += 1 
+                        m += 1 
+                    
+                    elif index1 != j and index2 == k and index1 != index2:
+                        n += 1 
+                        m += 1 
+                        
+                    else:
+                        m += 1
+                        
+                else:
+                    pass
+            print(f'{n}ヒット, {m}ブローだよ!')
+    
+    print(f'答えは{int(ans)}でした！')                    
+        
+hb(3)
