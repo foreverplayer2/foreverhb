3e33f66c37a9b0f7eabeec5edab23f34e08de891
towa <football.keepergrove2@gmail.com>
2024-02-08 19:32:27 +0900

    mergetraining1

--- hb.py
+++ hb.py
@@ -39,7 +39,7 @@ def hb(num):
             print(f'{n}ヒット, {m}ブローだよ!')
     
     print(f'答えは{int(ans)}でした！')     
-    print('mergeの練習')     
+    print('merge1の練習')     
                    
         
 hb(3)
